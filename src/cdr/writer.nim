826e52e1d4431f03dd3a6de34d586fc24e986828
Jaremy Creechley <creechley@gmail.com>
2022-12-03 15:47:10 -0700

    implementing

--- src/cdr/writer.nim
+++ src/cdr/writer.nim
@@ -38,10 +38,10 @@ proc writeBe*[T: SomeInteger|SomeFloat](s: Stream, x: T) =
   writeData(s, addr(tmp), sizeof(x))
 
 
-proc data*(this: CdrWriter): string =
+proc data(this: CdrWriter): string =
   return this.ss.data
 
-proc size*(this: CdrWriter, ): int =
+proc size(this: CdrWriter, ): int =
   return this.ss.getPosition()
 
 proc resizeIfNeeded(ss: StringStream, additionalBytes: int) =
@@ -49,7 +49,7 @@ proc resizeIfNeeded(ss: StringStream, additionalBytes: int) =
   if (ss.data.len() < capacity):
     ss.data.setLen(capacity)
 
-proc initCdrWriter(opts: CdrWriterOpts): CdrWriter =
+proc initCdrWriter*(opts: CdrWriterOpts): CdrWriter =
     new result
 
     if (opts.buffer.isSome):
@@ -73,3 +73,59 @@ proc initCdrWriter(opts: CdrWriterOpts): CdrWriter =
     # when it reads the options field
     result.ss.writeBe(0'u16)
     assert result.ss.getPosition() == 4
+
+proc align(this: CdrWriter, size: int, bytesToWrite: int = size): void =
+    let alignment = (this.ss.getPosition() - 4) mod size
+    let padding = if alignment > 0: size - alignment else: 0
+    echo "set alignment: ", size, " align: ", alignment, " to ", $(size-alignment)
+
+    this.ss.resizeIfNeeded(padding + bytesToWrite);
+    # // Write padding bytes
+    for i in 0 ..< padding:
+      this.ss.write(0'u8)
+
+
+template implWriter(NAME, TP, BS: untyped) =
+  proc `read NAME BS`*(this: CdrWriter): `TP BS` =
+    this.align(BS div 8)
+    when system.cpuEndian == littleEndian:
+      if this.littleEndian:
+        result = this.ss.`read NAME BS`()
+      else:
+        var tmp: `TP BS` = this.ss.`read NAME BS`()
+        `swapEndian BS`(addr(result), addr(tmp))
+    else: # bigendian
+      if this.littleEndian:
+        var tmp: `TP BS` = this.ss.`read NAME BS`()
+        `swapEndian BS`(addr(result), addr(tmp))
+      else:
+        result = this.ss.`read NAME BS`()
+
+implWriter(Uint, uint, 8)
+implWriter(Uint, uint, 16)
+implWriter(Uint, uint, 32)
+implWriter(Uint, uint, 64)
+
+implWriter(Int, int, 8)
+implWriter(Int, int, 16)
+implWriter(Int, int, 32)
+implWriter(Int, int, 64)
+
+implWriter(Float, float, 32)
+implWriter(Float, float, 64)
+
+template implWriterBe(NAME, TP, BS: untyped) =
+  proc `read NAME BS Be`*(this: CdrReader): `TP BS` =
+    this.align(BS div 8)
+    var tmp: `TP BS` = this.ss.`read NAME BS`()
+    bigEndian16(addr(result), addr(tmp))
+
+implWriterBe(Uint, uint, 8)
+implWriterBe(Uint, uint, 16)
+implWriterBe(Uint, uint, 32)
+implWriterBe(Uint, uint, 64)
+
+implWriterBe(Int, int, 8)
+implWriterBe(Int, int, 16)
+implWriterBe(Int, int, 32)
+implWriterBe(Int, int, 64)
