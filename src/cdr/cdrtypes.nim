48405112b962547dcfbc3e1a126089e9dace5c4b
Jaremy Creechley <creechley@gmail.com>
2022-12-03 17:28:47 -0700

    size

--- src/cdr/cdrtypes.nim
+++ src/cdr/cdrtypes.nim
@@ -70,3 +70,36 @@ proc readLe*[T: SomeInteger|SomeFloat](ss: Stream, x: typedesc[T]): T =
   else:
     error("unhandled size")
   
+type 
+  CdrSizeCalculator* = ref object
+    # Two bytes for Representation Id and two bytes for Options
+    offset: int # 4
+
+proc size(this: CdrSizeCalculator): int =
+  return this.offset
+
+proc incrementAndReturn(this: CdrSizeCalculator, byteCount: int): int =
+  # Increments the offset by `byteCount` and any required padding bytes and
+  # returns the new offset
+  let alignment = (this.offset - 4) mod byteCount
+  if (alignment > 0):
+    this.offset += byteCount - alignment
+  this.offset += byteCount;
+  return this.offset;
+
+proc write*[T: SomeInteger|SomeFloat](
+    this: CdrSizeCalculator,
+    tp: typedesc[T]
+): int {.discardable.} =
+  return this.incrementAndReturn(sizeof(T))
+
+proc writeStr*(
+    this: CdrSizeCalculator,
+    length: int
+): int =
+  this.write(uint32)
+  this.offset += length + 1 # Add one for the null terminator
+  return this.offset
+
+proc sequenceLength*(this: CdrSizeCalculator): int =
+  return this.write(uint32)
