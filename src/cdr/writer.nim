48e6df3a0233c082321632b9dc93c9e68a4ba063
Jaremy Creechley <creechley@gmail.com>
2022-12-03 17:02:20 -0700

    adding twriter tests

--- src/cdr/writer.nim
+++ src/cdr/writer.nim
@@ -5,87 +5,63 @@ import std/options
 
 import cdrtypes
 
+export options
+
 const
   DEFAULT_CAPACITY = 16
   BUFFER_COPY_THRESHOLD = 10
 
 type
   CdrWriterOpts* = object
-    buffer: Option[string]
-    size: Option[int]
-    kind: Option[EncapsulationKind]
 
   CdrWriter* = ref object
     littleEndian: bool
-    hostLittleEndian: bool
     buffer: string
     array: seq[uint8]
     ss: StringStream
 
-proc writeBe*[T: SomeInteger|SomeFloat](s: Stream, x: T) =
-  ## BigEndian version of generic write procedure. Writes `x` to the stream `s`. Implementation:
-  var tmp: T
-  when sizeof(T) == 1:
-    tmp = x
-  elif sizeof(T) == 2:
-    bigEndian16(tmp.addr, x.unsafeAddr)
-  elif sizeof(T) == 4:
-    bigEndian32(tmp.addr, x.unsafeAddr)
-  elif sizeof(T) == 8:
-    bigEndian64(tmp.addr, x.unsafeAddr)
-  else:
-    error("unhandled size")
-  writeData(s, addr(tmp), sizeof(x))
-
-proc writeLe*[T: SomeInteger|SomeFloat](s: Stream, x: T) =
-  ## BigEndian version of generic write procedure. Writes `x` to the stream `s`. Implementation:
-  var tmp: T
-  when sizeof(T) == 1:
-    tmp = x
-  elif sizeof(T) == 2:
-    bigEndian16(tmp.addr, x.unsafeAddr)
-  elif sizeof(T) == 4:
-    bigEndian32(tmp.addr, x.unsafeAddr)
-  elif sizeof(T) == 8:
-    bigEndian64(tmp.addr, x.unsafeAddr)
-  else:
-    error("unhandled size")
-  writeData(s, addr(tmp), sizeof(x))
-
-proc data(this: CdrWriter): string =
+proc data*(this: CdrWriter): string =
   return this.ss.data
 
-proc size(this: CdrWriter, ): int =
+proc getPosition*(this: CdrWriter, ): int =
   return this.ss.getPosition()
+
+proc isLittleEndian*(this: CdrWriter, ): bool =
+  return this.littleEndian
 
 proc resizeIfNeeded(ss: StringStream, additionalBytes: int) =
   let capacity = ss.getPosition() + additionalBytes
   if (ss.data.len() < capacity):
     ss.data.setLen(capacity)
 
-proc initCdrWriter*(opts: CdrWriterOpts): CdrWriter =
-    new result
+proc newCdrWriter*(
+    buffer: Option[string] = none(string),
+    size: Option[int] = none(int),
+    kind: Option[EncapsulationKind] = none(EncapsulationKind)
+): CdrWriter =
+  new result
 
-    if (opts.buffer.isSome):
-      result.ss = newStringStream(opts.buffer.get())
-    elif (opts.size.isSome):
-      result.ss = newStringStream(newString(opts.size.get()))
-    else:
-      result.ss = newStringStream(newString(DEFAULT_CAPACITY))
+  if (buffer.isSome):
+    result.ss = newStringStream(buffer.get())
+  elif (size.isSome):
+    result.ss = newStringStream(newString(size.get()))
+  else:
+    result.ss = newStringStream(newString(DEFAULT_CAPACITY))
 
-    let kind = if opts.kind.isSome: opts.kind.get()
-               else: EncapsulationKind.CDR_LE
-    result.littleEndian = kind in [CDR_LE, PL_CDR_LE]
+  let kind = if kind.isSome: kind.get()
+           else: EncapsulationKind.CDR_LE
+  result.littleEndian = kind in [CDR_LE, PL_CDR_LE]
+  echo "kind: ", kind
 
-    # Write the Representation Id and Offset fields
-    result.ss.write(0) # Upper bits of EncapsulationKind, unused
-    result.ss.write(kind)
+  # Write the Representation Id and Offset fields
+  result.ss.write(0'u8) # Upper bits of EncapsulationKind, unused
+  result.ss.write(kind.uint8)
 
-    # The RTPS specification does not define any settings for the 2 byte
-    # options field and further states that a receiver should not interpret it
-    # when it reads the options field
-    result.ss.writeBe(0'u16)
-    assert result.ss.getPosition() == 4
+  # The RTPS specification does not define any settings for the 2 byte
+  # options field and further states that a receiver should not interpret it
+  # when it reads the options field
+  result.ss.writeBe(0'u16)
+  assert result.ss.getPosition() == 4
 
 proc align(this: CdrWriter, size: int, bytesToWrite: int = size): void =
     let alignment = (this.ss.getPosition() - 4) mod size
@@ -114,7 +90,7 @@ proc write*(this: CdrWriter, value: string): CdrWriter {.discardable.} =
   this.ss.write(uint8(0)) # add null terminator
   return this
 
-proc sequenceLength(this: CdrWriter, value: int): CdrWriter {.discardable.} =
+proc sequenceLength*(this: CdrWriter, value: int): CdrWriter {.discardable.} =
   return this.write(uint32(value))
 
 proc write*[T: SomeInteger|SomeFloat|string](
---
+++ tests/twriter.nim
@@ -0,0 +1,55 @@
+# This is just an example to get you started. You may wish to put all of your
+# tests into a single file, or separate them into multiple `test1`, `test2`
+# etc. files (better names are recommended, just make sure the name starts with
+# the letter 't').
+#
+# To run these tests, simply execute `nimble test`.
+
+import unittest
+import strutils
+import stew/byteutils
+
+import cdr/cdrtypes
+import cdr/writer
+
+# Example tf2_msgs/TFMessage
+const tf2_msg_TFMessage: string =
+     "0001000001000000cce0d158f08cf9060a000000626173655f6c696e6b000000060000007261646172000000ae47e17a14ae0e4000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000f03f"
+
+proc near*[T: SomeFloat](x, y: T, eps: T): bool =
+  result = abs(x-y) < eps
+proc `~=` *[T: float64](x, y: T): bool =
+  near(x, y, 1.0e-6)
+proc `~=` *[T: float32](x, y: T): bool =
+  near(x, y, 1.0e-4)
+
+proc writeExampleMessage(writer: CdrWriter) =
+  # // geometry_msgs/TransformStamped[] transforms
+  writer.sequenceLength(1)
+  # // std_msgs/Header header
+  # // time stamp
+  writer.write uint32(1490149580) #// uint32 sec
+  writer.write uint32(117017840) #// uint32 nsec
+  writer.write "base_link" #// string frame_id
+  writer.write "radar" #// string child_frame_id
+  # // geometry_msgs/Transform transform
+  # // geometry_msgs/Vector3 translation
+  writer.write float64(3.835) #// float64 x
+  writer.write float64(0) #// float64 y
+  writer.write float64(0) #// float64 z
+  # // geometry_msgs/Quaternion rotation
+  writer.write float64(0) #// float64 x
+  writer.write float64(0) #// float64 y
+  writer.write float64(0) #// float64 z
+  writer.write float64(1) #// float64 w
+
+suite "CdrWriter":
+  test "serializes an example message with internal preallocation":
+    let data = $cast[string](tf2_msg_TFMessage.hexToSeqByte())
+    check tf2_msg_TFMessage == data.toHex().toLowerAscii()
+
+    var writer = newCdrWriter(size = some 100)
+    check writer.isLittleEndian() == true
+    writeExampleMessage(writer)
+    check(writer.getPosition() == 100)
+    check(toHex(writer.data) == tf2_msg_TFMessage)
